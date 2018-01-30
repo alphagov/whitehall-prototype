cfbc1f08a7447a2cc72aa3afa4f64fa300dc62b6
Paul Hayes <fofr@users.noreply.github.com>
2018-01-30 18:38:37 +0000

    Add proof of concept content checker
    Use `retext`:
    https://github.com/retextjs/retext

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var retext = require('../lib/retext.js')
 var router = express.Router()
 
 // Route index page
@@ -7,5 +8,12 @@ router.get('/', function (req, res) {
 })
 
 // add your routes here
+
+router.get('/patterns/content-checker', function(req, res) {
+  var text = req.session.data['text'];
+  var locals = {};
+  retext.checkText(text, locals);
+  res.render('patterns/content-checker', locals)
+})
 
 module.exports = router
