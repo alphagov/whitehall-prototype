8cdff46ce7a90d2abe9f3a4aec09ce90cba6b9e6
Paul Hayes <fofr@users.noreply.github.com>
2018-02-12 10:14:28 +0000

    Add back retext content checks

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const retext = require('../lib/retext.js')
 const router = express.Router()
 
 // Route index page
@@ -7,5 +8,29 @@ router.get('/', function (req, res) {
 })
 
 // Add your routes here - above the module.exports line
+
+router.get('/patterns/content-checker', function(req, res) {
+  var text = req.session.data['text'];
+  var locals = {};
+  retext.checkText(text, locals);
+  res.render('patterns/content-checker', locals)
+})
+
+router.get('/content-check', function(req, res){
+  checkContent(req, res, req.query['text']);
+});
+
+router.post('/content-check', function(req, res){
+  checkContent(req, res, req.body['text']);
+});
+
+function checkContent(req, res, text) {
+  var obj = {};
+
+  retext.checkText(text, obj);
+  res.header('Content-type','application/json');
+  res.header('Charset','utf8');
+  res.send(JSON.stringify(obj));
+}
 
 module.exports = router
